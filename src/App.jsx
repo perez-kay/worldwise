630b865b9246e39a68dacc3c2d9b64b00c778b74
Kayla Perez <kayla.n.perez404@gmail.com>
2024-09-06 15:11:19 -0700

    added some basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Product from './pages/Product';
+import Homepage from './pages/Homepage';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
+
 function App() {
-  return <div>Worldwise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
